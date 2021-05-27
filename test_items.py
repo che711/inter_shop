027765154c5465733a036b5eca61bfa42c27bb01
s20 <che7111987@gmail.com>
2021-05-27 10:52:21 +0300

    the end

--- test_items.py
+++ test_items.py
@@ -1,13 +1,16 @@
+import pytest
 import time
 
-def test_find_add_to_card_btn(browser):
+def test_button_bucket(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    time.sleep(30)
+    time.sleep(5)
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
-        'Кнопка добавления товара в корзину отсутсвует'
+        'Отсутствует кнопка "Добавить в корзину"'
 
 #    Команды для запуска теста:
 # pytest --language=es test_items.py  - для эстонского языка
 # pytest --language=fr test_items.py  - для французского языка
-# pytest --language=es --browser_name=firefox test_items.py  - эстонский язык для Firefox
-# pytest --language=es --browser_name=chrome test_items.py   - эстонский язык для Chrome
+
+if __name__ == "__main__":
+    pytest.main()
+
